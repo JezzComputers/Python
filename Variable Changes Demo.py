de76c8b55219e742e2be74919da8b1bab40b4027
Jeremy <jtspr4@schools.vic.edu.au>
2023-03-27 22:49:26 +1100

    Added line descriptions

--- Variable Changes Demo.py
+++ Variable Changes Demo.py
@@ -1,14 +1,14 @@
-from time import sleep
+from time import sleep #Importing sleep function from the time library
 
-item = 0
+item = 0 #Initialising the "item" varible
 
-while True:
-    if (item <= 9):
-        print(item)
-        item += 1
-        sleep(1)
-    else:
-        for a in range(10):
-            print(item)
-            item -= 1
-            sleep(1)
+while True: #Starting an infinite loop
+    if (item <= 9): #Check if varible value is less than or equal to 9
+        print(item) #Print "item" value in shell
+        item += 1 #Add 1 to "item" variable
+        sleep(1) #Sleep (wait) 1 second
+    else: #Else do
+        for a in range(10): #Loop 10 times
+            print(item) #Print "item" value in shell
+            item -= 1 #Take 1 from "item" variable
+            sleep(1) #Sleep (wait) 1 second
