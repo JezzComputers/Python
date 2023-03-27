5938baf8122c0a60c555450561268c239d723511
Jeremy <jtspr4@schools.vic.edu.au>
2023-03-27 20:26:01 +1100

    Update Variable Changes Demo.py

--- Variable Changes Demo.py
+++ Variable Changes Demo.py
@@ -1,11 +1,13 @@
+import time
+
 item = 0
 
 while True:
-
     while not (item > 9):
         print(item)
         item += 1
-    
+        time.sleep(1)
     while not (item < 1):
         print(item)
         item -= 1
+        time.sleep(1)
