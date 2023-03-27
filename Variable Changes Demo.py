755b37a3243ecdf6ebd5301d76f829d5da0d7203
Jeremy <jtspr4@schools.vic.edu.au>
2023-03-27 22:52:32 +1100

    Update Variable Changes Demo.py

--- Variable Changes Demo.py
+++ Variable Changes Demo.py
@@ -1,9 +1,9 @@
 from time import sleep #Importing sleep function from the time library
 
-item = 0 #Initialising the "item" varible
+item = 0 #Initialising the "item" variable
 
 while True: #Starting an infinite loop
-    if (item <= 9): #Check if varible value is less than or equal to 9
+    if (item <= 9): #Check if variable value is less than or equal to 9
         print(item) #Print "item" value in shell
         item += 1 #Add 1 to "item" variable
         sleep(1) #Sleep (wait) 1 second
