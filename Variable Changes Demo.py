799f95ff25f9626264e91b362617845ff782db4b
Jeremy <129046176+JezzComputers@users.noreply.github.com>
2023-03-27 20:17:49 +1100

    Update Variable Changes Demo.py

--- Variable Changes Demo.py
+++ Variable Changes Demo.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 item = 0
 
 while True:
@@ -7,9 +5,7 @@ while True:
     while not (item > 9):
         print(item)
         item += 1
-        sleep(0.5)
     
     while not (item < 1):
         print(item)
         item -= 1
-        sleep(0.5)
