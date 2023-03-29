bc7561bf1d3e459ef75422e3f5ad056e815324cc
Jeremy <jtspr4@schools.vic.edu.au>
2023-03-29 13:32:03 +1100

    Add files via upload

---
+++ Enter Username & Password.py
@@ -0,0 +1,11 @@
+print("All answers are in lowercase")
+userName = input ("What is your name? ")
+while userName != "jeremy":
+    print("You are not alowed in - try again!")
+    userName = input ("What is your name? ")
+print("You are alowed in.")
+userName = input ("What is your password? ")
+while userName != "thelakes":
+    print("You are not alowed in - try again!")
+    userName = input ("What is your password? ")
+print("You are alowed in!")
---
+++ Turtle draw.py
@@ -0,0 +1,8 @@
+import turtle
+import random
+pat = turtle.Turtle()
+turtle.Screen().bgcolor("white")
+while True:
+    pat.pendown()
+    pat.forward(100)
+    pat.backward(100)
