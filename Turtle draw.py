f20e70251f36fd463dc8f00bd79a1aa828ef2cb3
Jeremy <jtspr4@schools.vic.edu.au>
2023-04-01 16:31:22 +1100

    Update Turtle draw.py

--- Turtle draw.py
+++ Turtle draw.py
@@ -1,8 +1,8 @@
 import turtle
-import random
 pat = turtle.Turtle()
 turtle.Screen().bgcolor("white")
 while True:
     pat.pendown()
-    pat.forward(100)
-    pat.backward(100)
+    for i in range(4)
+        pat.forward(100)
+        pat.left()
