7ba6d253d72774f2f0b8dfe53fd9fd8d5cb11734
Jeremy <jtspr4@schools.vic.edu.au>
2023-05-31 11:42:25 +1000

    Update Requests Example.py

--- Requests Example.py
+++ Requests Example.py
@@ -1,1 +1,13 @@
+import json
+import random
+import requests
+from time import sleep
 
+while True:
+    num = random.randint(0,100)
+    requests.post('https://dweet.io/dweet/for/my_thing_name?', data={'value':num})
+    print(num)
+    value = requests.get('https://dweet.io/get/latest/dweet/for/my_thing_name')
+    value = value.json()
+    print(value)
+    sleep(3)
